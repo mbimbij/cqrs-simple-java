cd446d7a835ad58eb73c058b1bfe49187c8c201c
mbimbij <joseph.mbimbi@gmail.com>
2022-01-15 10:12:48 +0100

    Initial Commit

---
+++ src/test/java/org/example/cqrssimple/StarterTest.java
@@ -0,0 +1,13 @@
+package org.example.cqrssimple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StarterTest {
+    @Test
+    void name() {
+//        assertThat(true).isFalse();
+        assertThat(true).isTrue();
+    }
+}
